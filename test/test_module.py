74797abd95355df906f031b2a2140b462ef30560
silverlyjoo <silverlyjoo@gmail.com>
2021-03-01 17:30:51 +0900

    jobqueue test

--- test/test_module.py
+++ test/test_module.py
@@ -1,8 +1,1 @@
 
-import requests
-
-
-def work_test(self):
-        URL = "http://api.noti.daumkakao.io/send/group/kakaotalk"
-        params = {"from": "watchtower.bot", "to": 11859, "msg": "test_message", "ps": False}
-        res = requests.get(URL, params=params)
